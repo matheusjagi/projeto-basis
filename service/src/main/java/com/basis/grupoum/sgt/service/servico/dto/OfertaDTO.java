305bc573f8715d03f8c75c55151f4090fb3cb151
Gabriel <gabrielvicentinies@hotmail.com>
2021-05-03 20:32:10 -0300

    Atualizacao CrudOferta4

--- service/src/main/java/com/basis/grupoum/sgt/service/servico/dto/OfertaDTO.java
+++ service/src/main/java/com/basis/grupoum/sgt/service/servico/dto/OfertaDTO.java
@@ -1,8 +1,5 @@
 package com.basis.grupoum.sgt.service.servico.dto;
 
-import com.basis.grupoum.sgt.service.dominio.Item;
-import com.basis.grupoum.sgt.service.dominio.Situacao;
-import com.basis.grupoum.sgt.service.dominio.Usuario;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +11,12 @@ public class OfertaDTO {
 
     private Long id;
 
-    private Item item;
+    private Long itemDtoId;
 
-    private Usuario usuario;
+    private Long usuarioDtoId;
 
-    private Situacao situacao;
+    private Long situacaoDtoId;
 
-    private List<Item> itensOfertados;
+    private List<ItemDTO> itensOfertados;
 
 }
