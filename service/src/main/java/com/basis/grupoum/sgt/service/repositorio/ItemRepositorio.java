19729e2091d7140ed2b79b530f6288c96da93b82
Matheus Jagi <jagi.matheus@gmail.com>
2021-05-04 18:24:28 -0300

    codigo atualizado

--- service/src/main/java/com/basis/grupoum/sgt/service/repositorio/ItemRepositorio.java
+++ service/src/main/java/com/basis/grupoum/sgt/service/repositorio/ItemRepositorio.java
@@ -10,4 +10,8 @@ import java.util.List;
 public interface ItemRepositorio extends JpaRepository<Item, Long> {
 
     List<Item> findAllByDisponibilidade(boolean disponibilidade);
+
+    List<Item> findByNomeContaining(String nome);
+
+
 }
