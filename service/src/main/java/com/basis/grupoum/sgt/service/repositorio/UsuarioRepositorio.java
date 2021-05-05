0d9aaa0621a5b92f66bce35cfd8d3e6685622871
Matheus Jagi <jagi.matheus@gmail.com>
2021-05-04 21:10:28 -0300

    criacao da criptografia SHA2

--- service/src/main/java/com/basis/grupoum/sgt/service/repositorio/UsuarioRepositorio.java
+++ service/src/main/java/com/basis/grupoum/sgt/service/repositorio/UsuarioRepositorio.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-
     List<Usuario> findByNomeContaining(String nome);
 }
--- service/src/main/java/com/basis/grupoum/sgt/service/servico/UsuarioServico.java
+++ service/src/main/java/com/basis/grupoum/sgt/service/servico/UsuarioServico.java
@@ -11,6 +11,10 @@ import com.basis.grupoum.sgt.service.servico.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.UUID;
 
@@ -45,7 +49,7 @@ public class UsuarioServico {
 
     public UsuarioDTO salvar(UsuarioDTO usuarioDTO){
         Usuario usuario = usuarioMapper.toEntity(usuarioDTO);
-        usuario.setToken(UUID.randomUUID().toString());
+        usuario.setToken(criptografiaSHA2(usuario.getCpf()));
 
         //validar cpf
         //validar email
@@ -76,5 +80,21 @@ public class UsuarioServico {
         email.setDestinatario(usuario.getEmail());
 
         return email;
+    }
+
+    public String criptografiaSHA2(String cpf) {
+        String msgDecode = null;
+
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+            byte messageDigest[] = algorithm.digest(cpf.getBytes("UTF-8"));
+            msgDecode  = new String(messageDigest, "UTF-8");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return msgDecode;
     }
 }
