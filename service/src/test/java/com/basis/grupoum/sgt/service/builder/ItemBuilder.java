004c6769c458545f543ab6c3889b43955ad056aa
Gabriel <gabrielvicentinies@hotmail.com>
2021-05-06 20:13:18 -0300

    Teste de Itens

---
+++ service/src/test/java/com/basis/grupoum/sgt/service/builder/ItemBuilder.java
@@ -0,0 +1,46 @@
+package com.basis.grupoum.sgt.service.builder;
+
+import com.basis.grupoum.sgt.service.dominio.Categoria;
+import com.basis.grupoum.sgt.service.dominio.Item;
+import com.basis.grupoum.sgt.service.dominio.Usuario;
+import com.basis.grupoum.sgt.service.servico.ItemServico;
+import com.basis.grupoum.sgt.service.servico.dto.ItemDTO;
+import com.basis.grupoum.sgt.service.servico.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Base64;
+
+@Component
+public class ItemBuilder extends ConstrutorEntidade<Item> {
+
+    @Autowired
+    private ItemServico itemServico;
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Override
+    public Item construirEntidade() {
+        Item item = new Item();
+        item.setNome("Rudeus");
+        item.setDescricao("desc1");
+        String teste = "qual";
+        item.setFoto(Base64.getDecoder().decode(teste.getBytes(StandardCharsets.UTF_8)));
+        item.setDisponibilidade(true);
+        Categoria cat = new Categoria();
+        cat.setId(1L);
+        cat.setDescricao("desctest");
+        item.setCategoria(cat);
+
+        return item;
+    }
+
+    @Override
+    public Item persistir(Item entidade) {
+        ItemDTO itemDTO = itemMapper.toDto(entidade);
+        return itemMapper.toEntity(itemServico.salvar(itemDTO));
+    }
+}
---
+++ service/src/test/java/com/basis/grupoum/sgt/service/recurso/ItemRecursoIT.java
@@ -0,0 +1,78 @@
+package com.basis.grupoum.sgt.service.recurso;
+
+import com.basis.grupoum.sgt.service.builder.ItemBuilder;
+import com.basis.grupoum.sgt.service.dominio.Item;
+import com.basis.grupoum.sgt.service.repositorio.ItemRepositorio;
+import com.basis.grupoum.sgt.service.servico.mapper.ItemMapper;
+import com.basis.grupoum.sgt.service.util.IntTestComum;
+import com.basis.grupoum.sgt.service.util.TestUtil;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@Transactional
+public class ItemRecursoIT extends IntTestComum {
+
+    @Autowired
+    private ItemBuilder itemBuilder;
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Autowired
+    private ItemRepositorio itemRepositorio;
+
+    @BeforeEach
+    public void inicializar(){
+        itemRepositorio.deleteAll();
+        itemBuilder.setCustomizacao(null);
+    }
+
+    @Test
+    public void listar() throws Exception{
+        itemBuilder.construir();
+        getMockMvc().perform(get("/api/items"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(1)));
+    }
+
+    @Test
+    public void salvar() throws Exception{
+        Item item = itemBuilder.construirEntidade();
+        getMockMvc().perform(post("/api/items")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(itemMapper.toDto(item))))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void atualizar() throws Exception{
+        Item item = itemBuilder.construir();
+        item.setNome("Item Alterado");
+
+        getMockMvc().perform(put("/api/items")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(itemMapper.toDto(item))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void deletar() throws Exception{
+        Item item = itemBuilder.construir();
+
+        getMockMvc().perform(delete("/api/items/"+item.getId())
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(itemMapper.toDto(item))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
